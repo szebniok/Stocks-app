18a3931c1387cbbf85ab28a3a97f3bcf7930ba60
Konrad Bochnia <kbochnia@gmail.com>
2020-07-06 14:16:22 +0200

    Move Yahoo config to separate class

--- app/src/main/java/com/example/stocks/StockMarketRepository.java
+++ app/src/main/java/com/example/stocks/StockMarketRepository.java
@@ -3,42 +3,25 @@ package com.example.stocks;
 import org.androidannotations.annotations.EBean;
 
 import java.util.List;
-import java.util.Observable;
 import java.util.stream.Collectors;
 
 import io.reactivex.Single;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 @EBean
 public class StockMarketRepository {
-    private YahooWebservice yahooWebservice;
-    private static final String YahooApiKey = BuildConfig.YahooAPIKey;
-
-    public StockMarketRepository() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BuildConfig.YahooAPIHost)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build();
-
-        yahooWebservice = retrofit.create(YahooWebservice.class);
-    }
-
     public Single<List<Stock>> getSummary() {
-        return yahooWebservice.getSummary(YahooApiKey)
+        return YahooApiProvider.yahooWebservice.getSummary(YahooApiProvider.YahooApiKey)
                 .map(result -> result.marketSummaryResponse.result.stream()
                         .map(r -> r.toStock()).collect(Collectors.toList()));
     }
 
     public Single<Stock> getQuote(String symbol) {
         Single<QuotesResult.QuoteResponse.QuotesResultStock> quoteResponse =
-                yahooWebservice.getQuotes(YahooApiKey, symbol)
+                YahooApiProvider.yahooWebservice.getQuotes(YahooApiProvider.YahooApiKey, symbol)
                         .map(result -> result.quoteResponse.result.get(0));
 
         Single<ChartsResult.Chart.Result> chartResponse =
-                yahooWebservice.getCharts(YahooApiKey, symbol, "5m", "1d")
+                YahooApiProvider.yahooWebservice.getCharts(YahooApiProvider.YahooApiKey, symbol, "5m", "1d")
                     .map(result -> result.chart.result.get(0));
 
         return quoteResponse.zipWith(chartResponse, (quote, chart) -> {
---
+++ app/src/main/java/com/example/stocks/YahooApiProvider.java
@@ -0,0 +1,18 @@
+package com.example.stocks;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public final class YahooApiProvider {
+    public static final String YahooApiHost = "https://apidojo-yahoo-finance-v1.p.rapidapi.com";
+    public static final String YahooApiKey = "yhDyuXUP2HB27vnY5Z1FIaN7zuZMSYBy";
+
+    public static final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(YahooApiHost)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build();
+
+    public static final YahooWebservice yahooWebservice = retrofit.create(YahooWebservice.class);
+}
