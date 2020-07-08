200dcad44710d4970a101a5818ed2735578683fc
Konrad Bochnia <kbochnia@gmail.com>
2020-07-08 14:22:01 +0200

    Add TabLayout to ViewPager2

--- app/src/main/java/com/example/stocks/MainActivity.java
+++ app/src/main/java/com/example/stocks/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.stocks;
 
+import com.google.android.material.tabs.TabLayoutMediator;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -9,6 +11,7 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.example.stocks.stock_favourites.StockFavouritesFragment_;
 import com.example.stocks.stock_list.StockListFragment_;
 import com.example.stocks.stock_search.StockSearchFragment_;
+import com.google.android.material.tabs.TabLayout;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -19,12 +22,20 @@ public class MainActivity extends AppCompatActivity {
     @ViewById(R.id.pager)
     ViewPager2 viewPager;
 
+    @ViewById
+    TabLayout tabLayout;
+
     private FragmentStateAdapter adapter;
+
+    private static final String TABS_NAMES[] = {"Summary", "Search", "Favourites"};
 
     @AfterViews
     public void setup() {
         adapter = new MainActivityFragmentStateAdapter(this);
         viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) ->
+                tab.setText(TABS_NAMES[position])).attach();
     }
 
     private class MainActivityFragmentStateAdapter extends FragmentStateAdapter {
